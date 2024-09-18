0a8c79416b474fef7b2d1c248ec01d4a20f69284
Kishan_maurya <kishan.maurya@cashfree.com>
2024-09-18 12:42:41 +0530

    JS SDK integration with UPI app

--- PaymentScreen.js
+++ PaymentScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import CFWebView from './native/CFWebView';
 const PaymentScreen = ({}) => {
-    return (React.createElement(CFWebView, { paymentSessionId: "session_Nv8F6D2pKgc9XRF52yfmL7eNiLV0l6b2g5viqLpAr-zXFR1UISFTew9WoBAMFZevuhb45PNCcLvwBxORNYEonNRBlmCr7i7yreXs506rXJrT", style: { flex: 1 } }));
+    return (React.createElement(CFWebView, { paymentSessionId: "session_kY-tXlysx58bnK3kXrEagyeua0nZKynH9MlqgmqmVcRZVWT-GYKk3IgYF9wws47bDW7dScvudwkaG_BsbbfXFmrJn2Cpi4wAbAuaMIAbMmrU", style: { flex: 1 } }));
 };
 export default PaymentScreen;
