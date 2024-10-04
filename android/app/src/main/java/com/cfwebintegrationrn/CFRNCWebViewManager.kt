8ef0d940e636a425469cb65bb63c481c40b281c1
Kishan_maurya <kishan.maurya@cashfree.com>
2024-10-04 13:27:33 +0530

    Code Update with latest sessionId

--- android/app/src/main/java/com/cfwebintegrationrn/CFRNCWebViewManager.kt
+++ android/app/src/main/java/com/cfwebintegrationrn/CFRNCWebViewManager.kt
@@ -1,5 +1,11 @@
 package com.cfwebintegrationrn
 
+import android.app.Activity
+import android.content.Intent
+import android.webkit.WebView
+import com.facebook.react.bridge.ActivityEventListener
+import com.facebook.react.bridge.LifecycleEventListener
+import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.annotations.ReactModule
 import com.facebook.react.uimanager.ThemedReactContext
 import com.reactnativecommunity.webview.RNCWebView
@@ -8,9 +14,17 @@ import com.reactnativecommunity.webview.RNCWebViewManager
 import com.reactnativecommunity.webview.RNCWebViewWrapper
 
 @ReactModule(name = CFRNCWebViewManager.REACT_CLASS)
-class CFRNCWebViewManager : RNCWebViewManager() {
+class CFRNCWebViewManager(private val context: ReactApplicationContext) : RNCWebViewManager(),
+    ActivityEventListener, LifecycleEventListener {
     companion object {
         const val REACT_CLASS: String = "CFRNWebViewManager"
+    }
+
+    private var webView: WebView? = null
+
+    init {
+        context.addActivityEventListener(this)
+        context.addLifecycleEventListener(this)
     }
 
     private class CFWebViewClient : RNCWebViewClient()
@@ -21,7 +35,8 @@ class CFRNCWebViewManager : RNCWebViewManager() {
     }
 
     override fun createViewInstance(context: ThemedReactContext): RNCWebViewWrapper {
-        return super.createViewInstance(context, CFCustomWebView(context))
+        webView  = CFCustomWebView(context)
+        return super.createViewInstance(context, webView as CFCustomWebView)
     }
 
     override fun getName(): String = REACT_CLASS
@@ -31,5 +46,22 @@ class CFRNCWebViewManager : RNCWebViewManager() {
         viewWrapper: RNCWebViewWrapper
     ) {
         viewWrapper.webView.webViewClient = CFWebViewClient()
+    }
+
+    override fun onHostDestroy() {
+        context.removeActivityEventListener(this)
+        context.removeLifecycleEventListener(this)
+    }
+
+    override fun onHostPause() {}
+
+    override fun onHostResume() {}
+    override fun onActivityResult(p0: Activity?, p1: Int, p2: Int, p3: Intent?) {
+        if (p1 == 1000 && p2 == Activity.RESULT_OK && webView !=null) {
+            webView?.evaluateJavascript("window.showVerifyUI()") {}
+        }
+    }
+
+    override fun onNewIntent(p0: Intent?) {
     }
 }
